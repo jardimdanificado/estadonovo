fc33ad12c9385afae65adda9e818c68493bfc166
jardimdanificado <jardeltorreslima01@gmail.com>
2022-07-01 19:20:41 -0300

    unified savegame.c into 1964.c

--- src/1964/1964.c
+++ src/1964/1964.c
@@ -287,12 +287,6 @@ Camera MQCameraStart(Camera *camera)
 }
 
 //-----------------------------------
-//BULLETS
-//-----------------------------------
-
-//#include "core/bullet.c"
-
-//-----------------------------------
 //MODELS
 //-----------------------------------
 
@@ -308,7 +302,21 @@ Camera MQCameraStart(Camera *camera)
 //SAVE
 //-----------------------------------
 
-#include "savegame.c"
+void MQLoadGame(DATA *data)
+{
+    FILE *file;
+    file = fopen("data/save/savegame", "r+b");
+    fread(&data->game, sizeof(struct DATA_GAME), 1, file);
+    fclose(file);
+}
+
+void MQSaveGame(DATA data)
+{
+    remove("data/save/savegame");
+    FILE *file = fopen("data/save/savegame", "w+b");
+    fwrite(&data.game, sizeof(struct DATA_GAME), 1, file);
+    fclose(file);
+}
 
 //-----------------------------------
 //RENDER
