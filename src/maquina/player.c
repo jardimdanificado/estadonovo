5c52ac8228b7f316f1d16c11662a14e75aef4469
jardimdanificado <jardeltorreslima01@gmail.com>
2022-07-28 03:18:30 -0300

    MQPlayerPickupItem() added

--- src/maquina/player.c
+++ src/maquina/player.c
@@ -250,3 +250,40 @@ Vector3 MQPlayerMove(Vector3 position, float rotation, float speed)
     return position;
 }
 
+void MQPlayerPickupItem(MQDATA*data,int quem)
+{
+    int mapboxindex;
+    char bufferLocal[64];
+    snprintf(bufferLocal,64,"playeruse%d",quem);
+    mapboxindex = MQVerifyMapItemColision(*data, MQHitboxUpdateXYZ(data->game.event[MQFindEvent(*data,bufferLocal)].hitbox, data->game.event[MQFindEvent(*data,bufferLocal)].position));
+    if(mapboxindex!=-1)
+    {
+        int localIndex=-1;
+        for(int i = 0;i<MAXOBJ;i++)
+        {
+            if(strcmp(data->game.player[quem].inventory.equip[i].name," ")==0)
+            {
+                localIndex = i;
+                break;
+            }
+        }
+
+        MQDATA_GAME_ITEM item = MQCreateItem(data->game.map.item[mapboxindex].name,data->game.map.item[mapboxindex].type,data->game.map.item[mapboxindex].index,data->game.map.item[mapboxindex].position,data->game.map.item[mapboxindex].rotation,data->game.map.item[mapboxindex].condition,data->game.map.item[mapboxindex].content,data->game.map.item[mapboxindex].function,data->game.map.item[mapboxindex].hitbox,data->game.map.item[mapboxindex].locked,data->game.map.item[mapboxindex].active);
+
+        if(localIndex!=-1&&data->game.map.item[mapboxindex].active==true&&strcmp(data->game.map.item[mapboxindex].name," ")==0)
+        {
+            if(strcmp(data->game.map.item[mapboxindex].type,"calca")==0&&strcmp(data->game.map.item[mapboxindex].type,"camisa")==0&&strcmp(data->game.map.item[mapboxindex].type,"chapeu")==0&&strcmp(data->game.map.item[mapboxindex].type,"sapatos")==0&&strcmp(data->game.map.item[mapboxindex].type,"oculos")==0&&strcmp(data->game.map.item[mapboxindex].type,"arma")==0)
+            {
+                MQAddEquipToPlayerInventory(*&data,quem,item);
+            }
+            else
+            {
+                MQAddItemToPlayerInventory(*&data,quem,item);
+            }
+        }
+        else
+        {
+            MQAddItemToPlayerInventory(*&data,quem,item);
+        }
+    }
+}
--- src/maquina/teclado.c
+++ src/maquina/teclado.c
@@ -1,4 +1,3 @@
-
 void TECLADO_MAIN(MQDATA *data)
 {
     if(IsKeyPressed(KEY_ESCAPE))
@@ -7,39 +6,7 @@ void TECLADO_MAIN(MQDATA *data)
     }
     if(IsKeyPressed(KEY_E) || IsKeyPressed(KEY_E))
     {
-        int mapboxindex;
-
-        mapboxindex = MQVerifyMapItemColision(*data, MQHitboxUpdateXYZ(data->game.event[MQFindEvent(*data,"playeruse0")].hitbox, data->game.event[MQFindEvent(*data,"playeruse0")].position));
-        if(mapboxindex!=-1)
-        {
-            int localIndex=-1;
-            for(int i = 0;i<MAXOBJ;i++)
-            {
-                if(strcmp(data->game.player[0].inventory.equip[i].name," ")==0)
-                {
-                    localIndex = i;
-                    break;
-                }
-            }
-
-            MQDATA_GAME_ITEM item = MQCreateItem(data->game.map.item[mapboxindex].name,data->game.map.item[mapboxindex].type,data->game.map.item[mapboxindex].index,data->game.map.item[mapboxindex].position,data->game.map.item[mapboxindex].rotation,data->game.map.item[mapboxindex].condition,data->game.map.item[mapboxindex].content,data->game.map.item[mapboxindex].function,data->game.map.item[mapboxindex].hitbox,data->game.map.item[mapboxindex].locked,data->game.map.item[mapboxindex].active);
-
-            if(localIndex!=-1&&data->game.map.item[mapboxindex].active==true&&strcmp(data->game.map.item[mapboxindex].name," ")==0)
-            {
-                if(strcmp(data->game.map.item[mapboxindex].type,"calca")==0&&strcmp(data->game.map.item[mapboxindex].type,"camisa")==0&&strcmp(data->game.map.item[mapboxindex].type,"chapeu")==0&&strcmp(data->game.map.item[mapboxindex].type,"sapatos")==0&&strcmp(data->game.map.item[mapboxindex].type,"oculos")==0&&strcmp(data->game.map.item[mapboxindex].type,"arma")==0)
-                {
-                    MQAddEquipToPlayerInventory(*&data,0,item);
-                }
-                else
-                {
-                    MQAddItemToPlayerInventory(*&data,0,item);
-                }
-            }
-            else
-            {
-                MQAddItemToPlayerInventory(*&data,0,item);
-            }
-        }
+        MQPlayerPickupItem(*&data,0);
     } 
     if(IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
     {
