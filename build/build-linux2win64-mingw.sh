db4f25d41be96873ce2a6876f0b60e45eb6fbbd4
jardimdanificado <jardeltorreslima01@gmail.com>
2022-08-31 18:58:57 -0300

    declouped builder

--- build/build-linux2win64-mingw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-current_time=$(date "+%Y-%m-%d-%H:%M:%S")
-
-x86_64-w64-mingw32-gcc ../src/estadonovo.c -o ./estadonovo.exe -L ./libs/win64/ -lraylib -lopengl32 -lgdi32 -lwinmm
--- build/build-win64.bat
+++
@@ -1,1 +0,0 @@
-./compilers/mingw/bin/gcc.exe ../src/estadonovo.c -o ./estadonovo.exe -L ./libs/win64 -lraylib -lopengl32 -lgdi32 -lwinmm
