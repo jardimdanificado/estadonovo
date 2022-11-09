93d14794f2f758389cfe1d7c0cdd1f0168fda66c
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2022-11-09 09:59:58 +0000

    MINOR CHANGES

--- estadonovo.js
+++ estadonovo.js
@@ -4,9 +4,7 @@
 
 function getLength(input)
 {
-	//console.log(input)
 	let tempn=0;
-
 	while(true)
 	{
 		if(input[tempn])
@@ -115,11 +113,10 @@ function Vector3Max(v1,v2)
 
 function GetMeshBoundingBox(mesh)
 {
-	//console.log(mesh)
     // Get min and max vertex to construct bounds (AABB)
     let minVertex = { x:0,y:0,z:0 };
     let maxVertex = { x:0,y:0,z:0 };
-	
+
     if (mesh.vertices)
     {
         minVertex = mesh.vertices[0];
@@ -199,14 +196,14 @@ function keyPressed()
 		player.model = sistema.file.model['player-walk'];
 		player.currentProgression = -1;
 		if(frameCount%10)
-			sistema.render.scene.camera.update();
+			sistema.scene.camera.update();
 	}
 	else if (keyCode == 87 || keyCode == UP_ARROW ) //W
 	{
 		player.model = sistema.file.model['player-walk'];
 		player.currentProgression = 1;
 		if(frameCount%10)
-			sistema.render.scene.camera.update();
+			sistema.scene.camera.update();
 	}
 }
 
@@ -217,7 +214,7 @@ function keyReleased()
 		keyCode == 87 ||
  		keyCode == UP_ARROW || 
 		keyCode == DOWN_ARROW 
-	   )
+	   ) 
 	{
 		player.model = sistema.file.model['player-idle'];
 	}
@@ -252,7 +249,6 @@ var player;
 var camera;
 var map;
 var canvas;
-
 function preload()
 {
 	sistema = new Sistema();
@@ -289,48 +285,34 @@ function preload()
 
 function setup()
 {
+	//canvas
 	sistema.setup();
 	delete sistema.setup();
-
+	//delete sistema._cameraSetup();
+	
 	sistema.world.creature.new
 	({
 		name:'joao',
 		color:{r:140,g:100,b:0,a:255},
 		position:{x:0,y:0,z:0.5},
-		rotation:{x:180,y:180,z:0},
 		scale:{x:1,y:1,z:1},
-		model:sistema.file.model['player-idle']
+		model:sistema.file.model['player-idle'],
+		hitbox:{}
 	});
 	
 	player = sistema.world.creature['joao'];
-	sistema.render.scene.camera.target = player.position;
+	sistema.scene.camera.target = player.position;
 	map = 
 	{
 		name:'mapa0',
 		color:{r:0,g:0,b:0,a:0},
 		position:{x:0,y:0,z:0},
-		rotation:{x:180,y:180,z:0},
 		scale:{x:1,y:1,z:1},
 		model:sistema.file.model['map0'],
 		texture:sistema.file.image['map0']
 	}
-
-	sistema.file.hitbox.fromModel
-	({
-		...sistema.file.model['lvl0hitboxes'],
-		name:'lvl0'
-	});
-	
-	sistema.render.scene.model.add(map);
-	sistema.render.scene.model.add(player);
-	//sistema.render.scene.hitbox.add(player.hitbox);
-
-	for(i = 0;i<sistema.file.hitbox.length;i++)
-	{
-		sistema.file.hitbox[i].rotation = {x:180,y:180,z:0};
-		sistema.render.scene.hitbox.add(sistema.file.hitbox[i]);
-	}
-		
+	sistema.scene.model.add(map);
+	sistema.scene.model.add(player);
 }
 
 function draw() 
@@ -338,13 +320,8 @@ function draw()
 	sistema.gfx.clear();
 	sistema.gfx.noStroke();
 	keyDown();
-
-	for(i = 0; i < sistema.render.scene.hitbox.length;i++)
-		if(player.hitbox != sistema.render.scene.hitbox[i])
-			if(CheckCollisionBoxes(player.hitbox,sistema.render.scene.hitbox[i]))
-				print("colidiu");
 	
-	sistema.render.scene.render();
+	sistema.scene.render();
 	image(sistema.gfx, 0, 0, sistema.screen.w, sistema.screen.h);
 	
 	//mouse
@@ -354,4 +331,5 @@ function draw()
 	noFill();
 	circle(mouseX,mouseY,6);
 	pop();
+	
 }
--- maqquina.js
+++ maqquina.js
@@ -8,41 +8,6 @@ function fileStartup()
 	file.model = [];
 	file.image = [];
 	file.font = [];
-	file.hitbox = [];
-
-	file.hitbox.fromModel = function (filepath, iname) 
-	{
-		if (typeof filepath === 'object') 
-		{
-			/*
-   			when importing a hitbox collection
-	 		ref must follow this template:
-		 	{
-				...[model,model,model,model],
-				name:'lvl0hitboxes'
-			}
-			*/
-			let lobj = [];
-			let tempn = getLength(filepath);
-
-			if(!filepath.ext)
-				filepath.ext = '.obj';
-			
-			for (k = 0; k < tempn; k++)
-			{
-				lobj.push(GetMeshBoundingBox(filepath[k]));
-				this.push(lobj[k]);
-			}
-			this[filepath.name] = lobj;
-		}
-		else if (iname) 
-		{
-			this[iname] = GetMeshBoundingBox(loadModel(filepath));
-			this.push(this[iname]);
-		}
-		else
-			this.push(GetMeshBoundingBox(loadModel(filepath)));
-	};
 
 	file.model.load= function (filepath, iname) 
 	{
@@ -219,13 +184,11 @@ function worldStartup()
 //------------------------------------------
 //SISTEMA.RENDER
 //------------------------------------------
-var buff;
-function renderStartup()
+
+function sceneStartup()
 {
-	let render = [];
-	render.scene = [];
-	
-	render.scene.background =
+	let scene = [];
+	scene.background =
 	{
 		r: 1,
 		g: 1,
@@ -233,105 +196,25 @@ function renderStartup()
 		a: 1
 	};
 
-	render.scene.model = [];
-	render.scene.image = [];
-	render.scene.text = [];
-	render.scene.shape = [];
-	render.scene.hitbox = [];
+	scene.model = [];
+	scene.image = [];
+	scene.text = [];
+	scene.shape = [];
 	
-	render.scene.hitbox.add = function(ref)
-	{
-		/*
-	 	ref must follow this template:
-	 	{
-   			name:'this hitbox name',
-			min:{x:0,y:0,z:0},
-			max:{x:0,y:0,z:0}
-		}
-		*/
-		//this generate the new boundingbox if a queued model contains this hitbox
-		//else it can be generated via the aditional property
-		//console.log(ref)
-		ref.generate = function(mesh)
-		{
-			let tempb;
-
-			//if(!mesh)
-				//return false;
-			if(mesh.frame)
-				mesh.frame();
-			if(mesh[this.currentFrame])
-			{
-				tempb = (GetMeshBoundingBox(mesh[this.currentFrame]));
-				buff = tempb;
-			}
-			else
-				tempb = (GetMeshBoundingBox(mesh));
-			
-			tempb.min = RotateVerticeSelf(this.rotation.y, tempb.min);
-			tempb.max = RotateVerticeSelf(this.rotation.y, tempb.max);
-
-			//if(tempb.min.x > tempb.max.x && tempb.min.y > tempb.max.y && tempb.min.z > tempb.max.z)
-			tempb = {max:Vector3Max(tempb.min,tempb.max),min:Vector3Min(tempb.min,tempb.max)};
-			//buff = tempb;
-			tempb.min = Vector3Add(tempb.min,this.position);
-			tempb.max = Vector3Add(tempb.max,this.position);
-			
-			return(tempb);
-		}
-		
-		ref.update = function()
-		{
-			let temp = this.generate(this.model);
-			if(temp != false)//generate returns false if it cant generate a hitbox
-			{
-				//this.frame();
-				this.min = temp.min;
-				this.max = temp.max;
-			}
-		}
-		
-		ref.draw = function() 
-		{
-			render.scene.gfx.push();
-			render.scene.gfx.fill(0,255,0,100);
-			render.scene.gfx.translate(this.min.x,this.min.y*(-1),this.min.z);
-			render.scene.gfx.box
-			(
-				0.1,0.1,0.1
-			);
-			render.scene.gfx.pop();
-			
-			render.scene.gfx.push();
-			render.scene.gfx.translate(this.max.x,this.max.y*(-1),this.max.z);
-			render.scene.gfx.fill(255,0,0,100);
-			render.scene.gfx.box
-			(
-				0.1,0.1,0.1
-			);
-			render.scene.gfx.pop();
-		}
-		if(ref.name)
-		{
-			render.scene.hitbox[ref.name] = ref;
-		}
-		ref.active = true;
-		ref.visible = true;//debug
-		render.scene.hitbox.push(ref);
-	}
-	
-	render.scene.model.add = function(ref)
+	scene.model.add = function(ref)
 	{
 		/*
 	 	ref must follow this template:
 	 	{
 			name:'this model name',
-			color:{r:0,g:0,b:0,a:0},
-			position:{x:0,y:10,z:0},
-			rotation:{x:180,y:180,z:0},
-			scale:{x:10,y:10,z:10},
+			color:{r:0,g:0,b:0,a:0},//COLOR IS OPTIONAL THE DEFAULT IS {r:0,g:255,b:0,a:255}
+			position:{x:0,y:10,z:0},//POSITION iS OPTIONAL THE DEFAULT IS {x:0,y:0,z:0}
+			rotation:{x:180,y:180,z:0},//ROTATION IS OPTIONAL THE DEFAULT IS {x:180,y:180,z:0}
+			scale:{x:10,y:10,z:10},//SCALE S OPTIONAL THE DEFAULT IS {x:1,y:1,z:1}
 			model:sistema.file.model['model name'],
-			texture:sistema.file.image['text name']//TEXTURE IS OPTIONAL
+			texture:sistema.file.image['text name']//TEXTURE IS OPTIONAL,
+   			active: true, //active is optional DEFAULT IS TRUE
+	  		visible: true // visible is optional DEFAULT IS TRUE
 		}
 		*/
 		if(this[ref.name])
@@ -339,51 +222,89 @@ function renderStartup()
 			delete this[ref.name];
 		}
 
+		if(!ref.position)
+			ref.position = {x:0,y:0,z:0};
+		
+		if(!ref.rotation)
+			ref.rotation = {x:180,y:180,z:0};
+
+		if(!ref.scale)
+			ref.scale = {x:1,y:1,z:1};
+
+		if(!ref.color)
+			ref.color = {r:0,g:255,b:0,a:255};
+
+		if(!ref.active)
+			ref.active = true;
+
+		if(!ref.visible)
+			ref.visible = true;
+		
 		if(ref.model[0])
 		{
 			ref.currentFrame = 0;
 			ref.currentProgression = 1;
-			ref.frame = function()
-			{
-					if(this.currentFrame + this.currentProgression > getLength(this))
-					{
-						this.currentFrame = 0;
-					}
-					else if(this.currentFrame + this.currentProgression < 0)
-					{
-						this.currentFrame = getLength(this);
-					}
-					else
-					{
-						this.currentFrame += this.currentProgression;
-					}
-			}
-			ref.model.frame = ref.frame;
 		}
-		ref.active = true;
-		if(!ref.hitbox)
-			ref.hitbox = GetMeshBoundingBox(ref.model);
-		
-		ref.hitbox = 
+
+		ref.drawHitbox = function() 
 		{
-			min:Vector3Min(ref.hitbox.min,ref.hitbox.max),
-			max:Vector3Max(ref.hitbox.min,ref.hitbox.max),
-			rotation:ref.rotation,
-			position:ref.position,
-			model:ref.model
-		};
+			scene.gfx.push();
+	
+			if(CheckCollisionBoxes(ref.hitbox,{min:{x:-2,y:-2,z:-2},max:{x:2,y:2,z:2}}))
+			{
+				console.log("colidiu");
+			}
+				
+			scene.gfx.fill(0,255,0,100);
+			scene.gfx.translate(this.hitbox.min.x,this.hitbox.min.y*(-1),this.hitbox.min.z);
+			scene.gfx.box
+			(
+				0.1,0.1,0.1
+			);
+			scene.gfx.pop();
+			
+			scene.gfx.push();
+			scene.gfx.translate(this.hitbox.max.x,this.hitbox.max.y*(-1),this.hitbox.max.z);
+			scene.gfx.fill(255,0,0,100);
+			scene.gfx.box
+			(
+				0.1,0.1,0.1
+			);
+			scene.gfx.pop();
+		}
 		
-		render.scene.hitbox.add(ref.hitbox);
-		if(ref.frame)
-			ref.hitbox.frame = ref.frame;
-		//ref.hitboxUpdate();
-		ref.hitbox.update();
+		ref.getHitbox = function()//this get the ref 
+		{
+			let tempb;
+			
+			if(!ref.model[0])
+				tempb = (GetMeshBoundingBox(ref.model));
+			else
+				tempb = (GetMeshBoundingBox(ref.model[ref.currentFrame]))
+
+			tempb.min = RotateVerticeSelf(this.rotation.y, tempb.min);
+			tempb.max = RotateVerticeSelf(this.rotation.y, tempb.max);
+
+			//if(tempb.min.x > tempb.max.x && tempb.min.y > tempb.max.y && tempb.min.z > tempb.max.z)
+			tempb = {max:Vector3Max(tempb.min,tempb.max),min:Vector3Min(tempb.min,tempb.max)};
+			tempb.min = Vector3Add(tempb.min,this.position);
+			tempb.max = Vector3Add(tempb.max,this.position);
+			return(tempb);
+		}
+		
+		ref.updateHitbox = function()
+		{
+			let temp = this.getHitbox();
+			hitbox.min = temp.min;
+			hitbox.max = temp.max;
+		}
+		ref.hitbox = ref.getHitbox();
 		
 		this[ref.name] = ref;
 		this.push(ref);
 	}
 	
-	render.scene.text.add = function(ref)
+	scene.text.add = function(ref)
 	{
 		/*
 	 	ref must follow this template:
@@ -401,7 +322,7 @@ function renderStartup()
 		this.push(this[ref.name]);
 	}
 	
-	render.scene.image.add = function(ref)
+	scene.image.add = function(ref)
 	{
 		/* 	ref must follow this template:
 	 	{
@@ -417,7 +338,7 @@ function renderStartup()
 		this.push(this[ref.name]);
 	}
 	
-	render.scene.shape.add = function(ref)
+	scene.shape.add = function(ref)
 	{
 		//ref must contains at least: name, color, position(vec2), scale(vec2 but use wh instead xy)
 		/*
@@ -436,7 +357,7 @@ function renderStartup()
 		this.push(this[ref.name]);
 	}
 	
-	render.scene.render = function()
+	scene.render = function()
 	{
 		//CAMERA
 		//this.camera.setPosition(this.camera.position.x, this.camera.position.y, this.camera.position.z);
@@ -450,9 +371,23 @@ function renderStartup()
 		for(i=0;i<this.model.length;i++)
 			if(this.model[i].active)
 			{
-				if(this.model[i].frame && (frameCount%floor(60/24)==0 || this.model[i].currentFrame > this.model[i].model.length || this.model[i].currentFrame < 0))
+				if(frameCount%floor(60/24)==0 || this.model[i].currentFrame > this.model[i].model.length)
 				{
-					this.model[i].frame();
+					if(this.model[i].model[0])//IF ANIMATED
+					{
+						if(this.model[i].currentFrame + this.model[i].currentProgression > this.model[i].model.length-1)
+						{
+							this.model[i].currentFrame = 0;
+						}
+						else if(this.model[i].currentFrame + this.model[i].currentProgression < 0)
+						{
+							this.model[i].currentFrame = this.model[i].model.length-1;
+						}
+						else
+						{
+							this.model[i].currentFrame += this.model[i].currentProgression;
+						}
+					}
 				}
 				
 				this.gfx.push(); // Start a new drawing state
@@ -473,12 +408,14 @@ function renderStartup()
 					this.gfx.model(this.model[i].model);
 				this.gfx.pop();
 
-				this.model[i].hitbox.update();
+				if(this.model[i].hitbox)
+				{
+					this.model[i].drawHitbox();
+					this.model[i].hitbox = this.model[i].getHitbox();
+				}
+				
+				scene.gfx.box(4,4,4);//PLEASE REMOVE THIS
 			}
-		//HITBOX RENDERING
-		for(i=0;i<this.hitbox.length;i++)
-			if(this.hitbox[i].active && this.hitbox[i].visible)
-				this.hitbox[i].draw();
 		//TEXT RENDERING
 		for(i=0;i<this.text.length;i++)
 			if(this.text[i].active)
@@ -503,7 +440,7 @@ function renderStartup()
 				this.shape[i].shape(this.shape[i].position.x, this.shape[i].position.y, this.shape[i].scale.w, this.shape[i].scale.h);
 			}
 	}
-	return render;
+	return scene;
 }
 
 
@@ -526,7 +463,7 @@ class Sistema
 
 	file = fileStartup();
 	world = worldStartup();
-	render = renderStartup();
+	scene = sceneStartup();
 	
 	//canvas
 	canvas;//2D
@@ -536,14 +473,14 @@ class Sistema
 	{
 		this.canvas = createCanvas(sistema.screen.w, sistema.screen.h, P2D);//MAIN RENDERER USED FOR UI
 		this.gfx = createGraphics(sistema.screen.w, sistema.screen.h, WEBGL);//SECONDARY RENDERER USED FOR ALL 3D STUFF, THIS IS RENDERED IN MAIN RENDERER AS A IMAGE
-		this.render.scene.gfx = this.gfx;//reference to render be able to use 3D renderer
-		this.render.scene.model.background = this.background;//scene background color
+		this.scene.gfx = this.gfx;//reference to render be able to use 3D renderer
+		this.scene.model.background = this.background;//scene background color
 		//this applies NEAREST for all loaded images interpolation
 		this.gfx.noSmooth();
 		for(let i=0;i<this.file.image.length;i++)
 			this.gfx._renderer.getTexture(this.file.image[i]).setInterpolation(NEAREST, NEAREST);
 		this.gfx.perspective(PI / 3.0, sistema.screen.w / sistema.screen.h, 0.001, 1000);
-		this.gfx.background(this.render.scene.background.r,this.render.scene.background.g,this.render.scene.background.b,this.render.scene.background.a);//this sets the 3D scene background color
+		this.gfx.background(this.scene.background.r,this.scene.background.g,this.scene.background.b,this.scene.background.a);//this sets the 3D scene background color
 		this.gfx.frameRate(60);//3D renderer framerate
 		//noSmooth();
 		
@@ -551,15 +488,15 @@ class Sistema
 		frameRate(60);//main renderer framerate
 		this.gfx.angleMode(DEGREES);//set 3D renderer to use DEGREES instead RAD
 		angleMode(DEGREES);
-		this.render.scene.camera = this.gfx.createCamera();//create scene camera
-		this.render.scene.camera.position = {x:3,y:-10,z:10};//create scene camera position
-		this.render.scene.camera.target = {x:0,y:0,z:0};//where the camera will look at
-		this.render.scene.camera.update = function()
+		this.scene.camera = this.gfx.createCamera();//create scene camera
+		this.scene.camera.position = {x:3,y:-10,z:10};//create scene camera position
+		this.scene.camera.target = {x:0,y:0,z:0};//where the camera will look at
+		this.scene.camera.update = function()
 		{
 			this.setPosition(this.position.x, this.position.y, this.position.z);
 			this.lookAt(this.target.x,this.target.y,this.target.z);
 		}
-		this.render.scene.camera.update();
+		this.scene.camera.update();
 		this.gfx.textureWrap(CLAMP,CLAMP);	
 		
 
