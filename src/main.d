ad64e4fb7bbb526cda97806472acde1e55990ed9
jardimdanificado <jardeltorreslima01@gmail.com>
2023-11-16 18:23:25 -0300

    updated cordinates

--- src/main.d
+++ src/main.d
@@ -75,8 +75,167 @@ Vector3 move3d(Vector3 position, float rotation, float speed)
     }
     return position;
 }
+/*
+void UpdateGravityRay(targetobj)
+{
+    targetobj.ray.gravity.position = Vector3(targetobj.position.x,(targetobj.position.y+0.00001)-(0.005*(targetobj.fallTime)),targetobj.position.z);
+    targetobj.ray.gravity.direction = Vector3(0,1,0);
+}
 
-class CreatureNeeds
+void UpdateHorizontalRay(CreatureData* modelref, bool backwards)
+{
+    //z+ frente
+    //x+ esquerda
+    float valorZ;
+    float valorX;
+    if(modelref.rotation<=90.0)
+    {
+        if(backwards)
+        {
+            valorZ = -1.0+((1.0/90.0)*modelref.rotation);
+            valorX = 0.0-(1.0/90.0)*modelref.rotation;
+        }
+        else
+        {
+            valorZ = 1.0-((1.0/90.0)*modelref.rotation);
+            valorX = 0.0+(1.0/90.0)*modelref.rotation;
+        }
+    }
+    else if(modelref.rotation<=180)
+    {
+        if(backwards)
+        {
+            valorZ = 0.0+((1.0/90.0)*(modelref.rotation-90.0));
+            valorX = -1.0+((1.0/90.0)*(modelref.rotation-90.0));
+        }
+        else
+        {
+            valorZ = 0.0-((1.0/90.0)*(modelref.rotation-90.0));
+            valorX = 1.0-((1.0/90.0)*(modelref.rotation-90.0));
+        }
+    }
+    else if(modelref.rotation<=270)
+    {
+        if(backwards)
+        {
+            valorZ = 1.0-((1.0/90.0)*(modelref.rotation-180.0));
+            valorX = 0.0+((1.0/90.0)*(modelref.rotation-180.0));
+        }
+        else
+        {
+            valorZ = -1.0+((1.0/90.0)*(modelref.rotation-180.0));
+            valorX = 0.0-((1.0/90.0)*(modelref.rotation-180.0));
+        }
+    }
+    else 
+    {
+        if(backwards)
+        {
+            valorZ = 0.0-((1.0/90.0)*(modelref.rotation-270.0));
+            valorX = 1.0-((1.0/90.0)*(modelref.rotation-270.0));
+        }
+        else
+        {
+            valorZ = 0.0+((1.0/90.0)*(modelref.rotation-270.0));
+            valorX = -1.0+((1.0/90.0)*(modelref.rotation-270.0));
+        }
+    }
+    Vector3 newPosition = Vector3(modelref.position.x,modelref.position.y+0.7,modelref.position.z);
+    modelref.ray.horizontal.position.x = newPosition.x;
+    modelref.ray.horizontal.position.y = newPosition.y;
+    modelref.ray.horizontal.position.z = newPosition.z;
+    Vector3 newDirection = Vector3(valorX,0,valorZ);
+   	modelref.ray.horizontal.direction.x = newDirection.x;
+    modelref.ray.horizontal.direction.y = newDirection.y;
+    modelref.ray.horizontal.direction.z = newDirection.z;
+}
+
+float Gravity(targetobj, gravidade)
+{
+    return((targetobj.position.y) - gravidade*((targetobj.fallTime*(targetobj.fallTime/10)))/30);
+}
+
+void Gravit(WorldData world, targetobj)
+{
+	UpdateGravityRay(targetobj);
+	var gravityraiocolisao = {hit:false};
+    for(let i=0;i<data.scene.hitbox.length;i++)
+    {
+		if(data.scene.hitbox[i].active == true)
+		{
+			gravityraiocolisao = r.GetRayCollisionBox(targetobj.ray.gravity,data.scene.hitbox[i].hitbox);
+			if(gravityraiocolisao.hit == true)
+				break;
+			gravityraiocolisao.hit = false;
+		}
+    }
+	
+    if(gravityraiocolisao.hit == false)
+    {
+        targetobj.position.y = (Gravity(targetobj, 1));
+        targetobj.fallTime++;
+    }
+    else
+    {
+        if(gravityraiocolisao.distance<0.7)
+        {
+            targetobj.position.y = gravityraiocolisao.point.y;
+            targetobj.fallTime = 0;
+        }
+    }
+}
+
+bool PlayerCollider(data,modelref, backwards)
+{
+    var directionraiocolisao = {hit:false};
+    UpdateHorizontalRay(modelref,backwards);
+    for(let i=0;i<data.scene.hitbox.length;i++)
+		if(data.scene.hitbox[i].active == true)
+		{
+			directionraiocolisao = r.GetRayCollisionBox(modelref.ray.horizontal,data.scene.hitbox[i].hitbox);
+			if(directionraiocolisao.hit == true)
+			{
+				break;
+			}
+			directionraiocolisao.hit = false;
+		}
+    if(directionraiocolisao.hit == true)
+    {
+        if(directionraiocolisao.distance<0.5)
+        {
+            return true;
+        }
+        else
+        {
+            let caixa = {max:{...modelref.position},min:{...modelref.position}};
+            caixa.max.x += 0.2;
+            caixa.max.y += 1.2;
+            caixa.max.z += 0.2;
+            caixa.min.x += -0.2;
+            caixa.min.y += 0.8;
+            caixa.min.z += -0.2;
+            if(backwards)
+            {
+                Move3D(caixa.max,modelref.rotation,-0.1);
+                Move3D(caixa.min,modelref.rotation,-0.1);
+            }
+            else
+            {
+                Move3D(caixa.max,modelref.rotation,0.1);
+                Move3D(caixa.min,modelref.rotation,0.1);
+            }
+            for(let i = 0;i<data.scene.hitbox.length;i++)
+				if(data.scene.hitbox[i].active == true)
+					if(r.CheckCollisionBoxes(caixa,data.scene.hitbox[i].hitbox)==true)
+					{
+						return true;
+					}
+        }
+    }
+    return false;
+}*/
+
+struct CreatureNeeds
 {
     float hunger = 100;
     float thirst = 100;
@@ -98,6 +257,26 @@ class SpecimeData
     }
 }
 
+struct GenericRayData
+{
+    Vector3 position = {0, 0, 0};
+    Vector3 direction = {0, 0, 0};
+}
+
+private struct GenericObjectRayData
+{
+    GenericRayData gravity = GenericRayData();
+    GenericRayData horizontal = GenericRayData();
+}
+
+struct GenericData
+{
+    Vector3 position = {0, 0, 0};
+    float rotation = 0;
+    float speed = 0.25;
+    GenericObjectRayData ray =  GenericObjectRayData(GenericRayData(Vector3(0,0,0), Vector3(0,1,0)), GenericRayData(Vector3(0,0,0), Vector3(0,0,0)));
+}
+
 struct CreatureRenderData
 {
     int anim = 0;
@@ -110,13 +289,11 @@ struct CreatureData
 {
     string name;
     string specime = "human";
-    Vector3 position = {0, 0, 0};
-    float rotation = 0;
-    float speed = 0.25;
-    CreatureNeeds needs;
     int map = 0;
+    CreatureNeeds needs = CreatureNeeds();
     Color color = Color(155, 132, 15, 255);
     CreatureRenderData renderData = CreatureRenderData();
+    GenericData generic = GenericData();
 }
 
 class MapData
@@ -162,7 +339,7 @@ struct WorldData
 
     CreatureData* newCreature(string name, string specime = "human", Vector3 position = Vector3(0,0,0), float rotation = 0, float speed = 0.125)
     {
-        this.creatures ~= CreatureData(name, specime, position, rotation, speed);
+        this.creatures ~= CreatureData(name, specime);
         return &this.creatures[this.creatures.length - 1];
     }
 }
@@ -233,7 +410,7 @@ class RenderData
             if(key.map == this.worldPointer.player.creaturePointer.map)
             {
 
-                DrawModelEx(this.worldPointer.specimes[0].models[key.renderData.anim][key.renderData.frame], key.position, Vector3(0, 1, 0), (key.rotation), Vector3(1, 1, 1), key.color);
+                DrawModelEx(this.worldPointer.specimes[0].models[key.renderData.anim][key.renderData.frame], key.generic.position, Vector3(0, 1, 0), (key.generic.rotation), Vector3(1, 1, 1), key.color);
                 
                 bool tempCondition = false;
                 int tempFrame = 0;
@@ -270,12 +447,13 @@ class RenderData
 
     void render()
     {
-        camera.target = this.worldPointer.player.creaturePointer.position;
+        camera.target = this.worldPointer.player.creaturePointer.generic.position;
         BeginDrawing();
         bakeRenderTexture();
         
         DrawTextureEx(this.renderTexture.texture, Vector2(this.screen.x, this.screen.y), 180, this.pixelSize, Color(255, 255, 255, 255));
         DrawTextEx(this.defaultFont, "Estado Novo", Vector2(0, this.screen.y-17), 20, 0, Color(255, 255, 244, 255));
+        
         EndDrawing();
         this.sessionPointer.tick++;
     }
@@ -322,31 +500,31 @@ void main()
 
         if(IsKeyDown(68))
         {
-            player.creaturePointer.rotation += 3;
-            if(player.creaturePointer.rotation > 360)
+            player.creaturePointer.generic.rotation += 3;
+            if(player.creaturePointer.generic.rotation > 360)
             {
-                player.creaturePointer.rotation -= 360;
+                player.creaturePointer.generic.rotation -= 360;
             }
         }
         if(IsKeyDown(65))
         {
-            player.creaturePointer.rotation -= 3;
-            if(player.creaturePointer.rotation < 0)
+            player.creaturePointer.generic.rotation -= 3;
+            if(player.creaturePointer.generic.rotation < 0)
             {
-                player.creaturePointer.rotation += 360;
+                player.creaturePointer.generic.rotation += 360;
             }
         }
         if (IsKeyDown(87))
         {
-            Vector3 temp = move3d(player.creaturePointer.position, player.creaturePointer.rotation, player.creaturePointer.speed);
-            player.creaturePointer.position.x = temp.x;
-            player.creaturePointer.position.z = temp.z;
+            Vector3 temp = move3d(player.creaturePointer.generic.position, player.creaturePointer.generic.rotation, player.creaturePointer.generic.speed);
+            player.creaturePointer.generic.position.x = temp.x;
+            player.creaturePointer.generic.position.z = temp.z;
         }
         if (IsKeyDown(83))
         {
-            Vector3 temp = move3d(player.creaturePointer.position, player.creaturePointer.rotation, -player.creaturePointer.speed);
-            player.creaturePointer.position.x = temp.x;
-            player.creaturePointer.position.z = temp.z;
+            Vector3 temp = move3d(player.creaturePointer.generic.position, player.creaturePointer.generic.rotation, -player.creaturePointer.generic.speed);
+            player.creaturePointer.generic.position.x = temp.x;
+            player.creaturePointer.generic.position.z = temp.z;
         }
     };
     SetTargetFPS(to!int(config["framerate"].integer));
