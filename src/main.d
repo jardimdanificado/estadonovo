151e607bbc0fd899d8ec0b3fa040bd4101044ee8
jardimdanificado <jardeltorreslima01@gmail.com>
2023-11-16 19:32:21 -0300

    some collision functions adapted from javascript

--- src/main.d
+++ src/main.d
@@ -35,205 +35,34 @@ T[][] splitList(T)(T[] lista, size_t tamanho)
     return listasMenores;
 }
 
-Vector3 move3d(Vector3 position, float rotation, float speed)
+struct HitboxSlot
 {
-    //z+ frente
-    //x+ esquerda
-    float  valorZ, valorX;
-    int giro = to!int(floor(rotation / 90.0));
-    float resto = rotation - (90.0 * giro);
-    float restodoresto = 90.0 - resto;
-    valorZ = speed - (resto * (speed / 90.0));
-    valorX = (speed - (restodoresto * (speed / 90)));
-	switch(giro)
-    {
-        case 0:
-        {
-            position.z += valorZ;
-            position.x += valorX;
-        }
-        break;
-        case 1:
-        {
-            position.z -= valorX;
-            position.x += valorZ;
-        }
-        break;
-        case 2:
-        {
-            position.z -= valorZ;
-            position.x -= valorX;
-        }
-        break;
-        case 3:
-        {
-            position.z += valorX;
-            position.x -= valorZ;
-        }
-        break;
-        default:break;
-    }
-    return position;
-}
-/*
-void UpdateGravityRay(targetobj)
-{
-    targetobj.ray.gravity.position = Vector3(targetobj.position.x,(targetobj.position.y+0.00001)-(0.005*(targetobj.fallTime)),targetobj.position.z);
-    targetobj.ray.gravity.direction = Vector3(0,1,0);
+    BoundingBox hitbox;
+    bool active = true;
 }
 
-void UpdateHorizontalRay(CreatureData* modelref, bool backwards)
+private struct GenericObjectRayData
 {
-    //z+ frente
-    //x+ esquerda
-    float valorZ;
-    float valorX;
-    if(modelref.rotation<=90.0)
-    {
-        if(backwards)
-        {
-            valorZ = -1.0+((1.0/90.0)*modelref.rotation);
-            valorX = 0.0-(1.0/90.0)*modelref.rotation;
-        }
-        else
-        {
-            valorZ = 1.0-((1.0/90.0)*modelref.rotation);
-            valorX = 0.0+(1.0/90.0)*modelref.rotation;
-        }
-    }
-    else if(modelref.rotation<=180)
-    {
-        if(backwards)
-        {
-            valorZ = 0.0+((1.0/90.0)*(modelref.rotation-90.0));
-            valorX = -1.0+((1.0/90.0)*(modelref.rotation-90.0));
-        }
-        else
-        {
-            valorZ = 0.0-((1.0/90.0)*(modelref.rotation-90.0));
-            valorX = 1.0-((1.0/90.0)*(modelref.rotation-90.0));
-        }
-    }
-    else if(modelref.rotation<=270)
-    {
-        if(backwards)
-        {
-            valorZ = 1.0-((1.0/90.0)*(modelref.rotation-180.0));
-            valorX = 0.0+((1.0/90.0)*(modelref.rotation-180.0));
-        }
-        else
-        {
-            valorZ = -1.0+((1.0/90.0)*(modelref.rotation-180.0));
-            valorX = 0.0-((1.0/90.0)*(modelref.rotation-180.0));
-        }
-    }
-    else 
-    {
-        if(backwards)
-        {
-            valorZ = 0.0-((1.0/90.0)*(modelref.rotation-270.0));
-            valorX = 1.0-((1.0/90.0)*(modelref.rotation-270.0));
-        }
-        else
-        {
-            valorZ = 0.0+((1.0/90.0)*(modelref.rotation-270.0));
-            valorX = -1.0+((1.0/90.0)*(modelref.rotation-270.0));
-        }
-    }
-    Vector3 newPosition = Vector3(modelref.position.x,modelref.position.y+0.7,modelref.position.z);
-    modelref.ray.horizontal.position.x = newPosition.x;
-    modelref.ray.horizontal.position.y = newPosition.y;
-    modelref.ray.horizontal.position.z = newPosition.z;
-    Vector3 newDirection = Vector3(valorX,0,valorZ);
-   	modelref.ray.horizontal.direction.x = newDirection.x;
-    modelref.ray.horizontal.direction.y = newDirection.y;
-    modelref.ray.horizontal.direction.z = newDirection.z;
+    Ray gravity = Ray(Vector3(0,0,0), Vector3(0,0,0));
+    Ray horizontal = Ray(Vector3(0,0,0), Vector3(0,0,0));
 }
 
-float Gravity(targetobj, gravidade)
+struct GenericData
 {
-    return((targetobj.position.y) - gravidade*((targetobj.fallTime*(targetobj.fallTime/10)))/30);
+    Vector3 position = {0, 0, 0};
+    float rotation = 0;
+    float speed = 0.25;
+    float fallTime = 0;
+    GenericObjectRayData ray =  GenericObjectRayData(Ray(Vector3(0,0,0), Vector3(0,1,0)), Ray(Vector3(0,0,0), Vector3(0,0,0)));
 }
 
-void Gravit(WorldData world, targetobj)
+struct CreatureRenderData
 {
-	UpdateGravityRay(targetobj);
-	var gravityraiocolisao = {hit:false};
-    for(let i=0;i<data.scene.hitbox.length;i++)
-    {
-		if(data.scene.hitbox[i].active == true)
-		{
-			gravityraiocolisao = r.GetRayCollisionBox(targetobj.ray.gravity,data.scene.hitbox[i].hitbox);
-			if(gravityraiocolisao.hit == true)
-				break;
-			gravityraiocolisao.hit = false;
-		}
-    }
-	
-    if(gravityraiocolisao.hit == false)
-    {
-        targetobj.position.y = (Gravity(targetobj, 1));
-        targetobj.fallTime++;
-    }
-    else
-    {
-        if(gravityraiocolisao.distance<0.7)
-        {
-            targetobj.position.y = gravityraiocolisao.point.y;
-            targetobj.fallTime = 0;
-        }
-    }
+    int anim = 0;
+    int frame = 0;
+    int lastcheck = 0;
+    bool reverse = false;
 }
-
-bool PlayerCollider(data,modelref, backwards)
-{
-    var directionraiocolisao = {hit:false};
-    UpdateHorizontalRay(modelref,backwards);
-    for(let i=0;i<data.scene.hitbox.length;i++)
-		if(data.scene.hitbox[i].active == true)
-		{
-			directionraiocolisao = r.GetRayCollisionBox(modelref.ray.horizontal,data.scene.hitbox[i].hitbox);
-			if(directionraiocolisao.hit == true)
-			{
-				break;
-			}
-			directionraiocolisao.hit = false;
-		}
-    if(directionraiocolisao.hit == true)
-    {
-        if(directionraiocolisao.distance<0.5)
-        {
-            return true;
-        }
-        else
-        {
-            let caixa = {max:{...modelref.position},min:{...modelref.position}};
-            caixa.max.x += 0.2;
-            caixa.max.y += 1.2;
-            caixa.max.z += 0.2;
-            caixa.min.x += -0.2;
-            caixa.min.y += 0.8;
-            caixa.min.z += -0.2;
-            if(backwards)
-            {
-                Move3D(caixa.max,modelref.rotation,-0.1);
-                Move3D(caixa.min,modelref.rotation,-0.1);
-            }
-            else
-            {
-                Move3D(caixa.max,modelref.rotation,0.1);
-                Move3D(caixa.min,modelref.rotation,0.1);
-            }
-            for(let i = 0;i<data.scene.hitbox.length;i++)
-				if(data.scene.hitbox[i].active == true)
-					if(r.CheckCollisionBoxes(caixa,data.scene.hitbox[i].hitbox)==true)
-					{
-						return true;
-					}
-        }
-    }
-    return false;
-}*/
 
 struct CreatureNeeds
 {
@@ -257,34 +86,6 @@ class SpecimeData
     }
 }
 
-struct GenericRayData
-{
-    Vector3 position = {0, 0, 0};
-    Vector3 direction = {0, 0, 0};
-}
-
-private struct GenericObjectRayData
-{
-    GenericRayData gravity = GenericRayData();
-    GenericRayData horizontal = GenericRayData();
-}
-
-struct GenericData
-{
-    Vector3 position = {0, 0, 0};
-    float rotation = 0;
-    float speed = 0.25;
-    GenericObjectRayData ray =  GenericObjectRayData(GenericRayData(Vector3(0,0,0), Vector3(0,1,0)), GenericRayData(Vector3(0,0,0), Vector3(0,0,0)));
-}
-
-struct CreatureRenderData
-{
-    int anim = 0;
-    int frame = 0;
-    int lastcheck = 0;
-    bool reverse = false;
-}
-
 struct CreatureData
 {
     string name;
@@ -301,13 +102,17 @@ class MapData
     string name;
     JSONValue json;
     Model[] models;
-    BoundingBox[] hitboxes;
+    HitboxSlot[] hitboxes;
     this(string mapPath)
     {
         this.json = parseJSON(readTxt(mapPath ~ "map.json"));
         this.name = this.json["name"].str;
         this.models = bulkLoadModel(mapPath ~ "model/", to!int(this.json["model_count"].integer), this.json["model_extension"].str);
-        this.hitboxes = bulkLoadHitbox(mapPath ~ "hitbox/", to!int(this.json["hitbox_count"].integer), this.json["hitbox_extension"].str);
+        BoundingBox[] _hitboxes = bulkLoadHitbox(mapPath ~ "hitbox/", to!int(this.json["hitbox_count"].integer), this.json["hitbox_extension"].str);
+        foreach (key; _hitboxes)
+        {
+            this.hitboxes ~= HitboxSlot(key, true);
+        }
     }
 }
 
@@ -457,6 +262,214 @@ class RenderData
         EndDrawing();
         this.sessionPointer.tick++;
     }
+}
+
+
+Vector3 move3d(Vector3 position, float rotation, float speed)
+{
+    //z+ frente
+    //x+ esquerda
+    float  valorZ, valorX;
+    int giro = to!int(floor(rotation / 90.0));
+    float resto = rotation - (90.0 * giro);
+    float restodoresto = 90.0 - resto;
+    valorZ = speed - (resto * (speed / 90.0));
+    valorX = (speed - (restodoresto * (speed / 90)));
+	switch(giro)
+    {
+        case 0:
+        {
+            position.z += valorZ;
+            position.x += valorX;
+        }
+        break;
+        case 1:
+        {
+            position.z -= valorX;
+            position.x += valorZ;
+        }
+        break;
+        case 2:
+        {
+            position.z -= valorZ;
+            position.x -= valorX;
+        }
+        break;
+        case 3:
+        {
+            position.z += valorX;
+            position.x -= valorZ;
+        }
+        break;
+        default:break;
+    }
+    return position;
+}
+
+void UpdateGravityRay(GenericData* targetobj)
+{
+    targetobj.ray.gravity.position.x = targetobj.position.x;
+    targetobj.ray.gravity.position.y = (targetobj.position.y+0.00001)-(0.005*(targetobj.fallTime));
+    targetobj.ray.gravity.position.z = targetobj.position.z;
+    targetobj.ray.gravity.direction.x = 0;
+    targetobj.ray.gravity.direction.y = 1;
+    targetobj.ray.gravity.direction.z = 0;
+}
+
+void UpdateHorizontalRay(GenericData* targetobj, bool backwards)
+{
+    //z+ frente
+    //x+ esquerda
+    float valorZ;
+    float valorX;
+    if(targetobj.rotation<=90.0)
+    {
+        if(backwards)
+        {
+            valorZ = -1.0+((1.0/90.0)*targetobj.rotation);
+            valorX = 0.0-(1.0/90.0)*targetobj.rotation;
+        }
+        else
+        {
+            valorZ = 1.0-((1.0/90.0)*targetobj.rotation);
+            valorX = 0.0+(1.0/90.0)*targetobj.rotation;
+        }
+    }
+    else if(targetobj.rotation<=180)
+    {
+        if(backwards)
+        {
+            valorZ = 0.0+((1.0/90.0)*(targetobj.rotation-90.0));
+            valorX = -1.0+((1.0/90.0)*(targetobj.rotation-90.0));
+        }
+        else
+        {
+            valorZ = 0.0-((1.0/90.0)*(targetobj.rotation-90.0));
+            valorX = 1.0-((1.0/90.0)*(targetobj.rotation-90.0));
+        }
+    }
+    else if(targetobj.rotation<=270)
+    {
+        if(backwards)
+        {
+            valorZ = 1.0-((1.0/90.0)*(targetobj.rotation-180.0));
+            valorX = 0.0+((1.0/90.0)*(targetobj.rotation-180.0));
+        }
+        else
+        {
+            valorZ = -1.0+((1.0/90.0)*(targetobj.rotation-180.0));
+            valorX = 0.0-((1.0/90.0)*(targetobj.rotation-180.0));
+        }
+    }
+    else 
+    {
+        if(backwards)
+        {
+            valorZ = 0.0-((1.0/90.0)*(targetobj.rotation-270.0));
+            valorX = 1.0-((1.0/90.0)*(targetobj.rotation-270.0));
+        }
+        else
+        {
+            valorZ = 0.0+((1.0/90.0)*(targetobj.rotation-270.0));
+            valorX = -1.0+((1.0/90.0)*(targetobj.rotation-270.0));
+        }
+    }
+    Vector3 newPosition = Vector3(targetobj.position.x,targetobj.position.y+0.7,targetobj.position.z);
+    targetobj.ray.horizontal.position.x = newPosition.x;
+    targetobj.ray.horizontal.position.y = newPosition.y;
+    targetobj.ray.horizontal.position.z = newPosition.z;
+    Vector3 newDirection = Vector3(valorX,0,valorZ);
+   	targetobj.ray.horizontal.direction.x = newDirection.x;
+    targetobj.ray.horizontal.direction.y = newDirection.y;
+    targetobj.ray.horizontal.direction.z = newDirection.z;
+}
+
+float Gravity(GenericData targetobj, float gravidade)
+{
+    return((targetobj.position.y) - gravidade*((targetobj.fallTime*(targetobj.fallTime/10)))/30);
+}
+
+void Gravit(WorldData* world, GenericData* targetobj)
+{
+	UpdateGravityRay(targetobj);
+	RayCollision gravityraiocolisao = RayCollision(false,0,Vector3(0,0,0),Vector3(0,0,0));
+    HitboxSlot[] hitboxes = world.maps[world.player.creaturePointer.map].hitboxes;
+    foreach(key;hitboxes)
+    {
+		if(key.active == true)
+		{
+			gravityraiocolisao = GetRayCollisionBox(targetobj.ray.gravity, key.hitbox);
+			if(gravityraiocolisao.hit == true)
+				break;
+			gravityraiocolisao.hit = false;
+		}
+    }
+	
+    if(gravityraiocolisao.hit == false)
+    {
+        targetobj.position.y = (Gravity(*targetobj, 1));
+        targetobj.fallTime++;
+    }
+    else
+    {
+        if(gravityraiocolisao.distance<0.7)
+        {
+            targetobj.position.y = gravityraiocolisao.point.y;
+            targetobj.fallTime = 0;
+        }
+    }
+}
+
+bool PlayerCollider(WorldData* world,GenericData* targetobj, bool backwards)
+{
+    RayCollision directionraiocolisao = RayCollision(false,0,Vector3(0,0,0),Vector3(0,0,0));
+    UpdateHorizontalRay(targetobj,backwards);
+    HitboxSlot[] hitboxes = world.maps[world.player.creaturePointer.map].hitboxes;
+    foreach(key;hitboxes)
+		if(key.active == true)
+		{
+			directionraiocolisao = GetRayCollisionBox(targetobj.ray.horizontal,key.hitbox);
+			if(directionraiocolisao.hit == true)
+			{
+				break;
+			}
+			directionraiocolisao.hit = false;
+		}
+    
+    if(directionraiocolisao.hit == true)
+    {
+        if(directionraiocolisao.distance<0.5)
+        {
+            return true;
+        }
+        else
+        {
+            BoundingBox caixa = BoundingBox(targetobj.position,targetobj.position);
+            caixa.max.x += 0.2;
+            caixa.max.y += 1.2;
+            caixa.max.z += 0.2;
+            caixa.min.x += -0.2;
+            caixa.min.y += 0.8;
+            caixa.min.z += -0.2;
+            if(backwards)
+            {
+                move3d(caixa.max,targetobj.rotation,-0.1);
+                move3d(caixa.min,targetobj.rotation,-0.1);
+            }
+            else
+            {
+                move3d(caixa.max,targetobj.rotation,0.1);
+                move3d(caixa.min,targetobj.rotation,0.1);
+            }
+            foreach(key;hitboxes)
+				if(key.active == true)
+					if(CheckCollisionBoxes(caixa,key.hitbox)==true)
+					{
+						return true;
+					}
+        }
+    }
+    return false;
 }
 
 void main()
